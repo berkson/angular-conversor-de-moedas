a48fc99f861294b5e7101439b88293562395fff3
Berkson <berksonx@yahoo.com.br>
2021-12-14 22:41:05 -0300

    criado o serviço de fornecimento de listagem de moedas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'conversor-de-moedas'`, () => {
+  it(`should have as title 'Conversor de Moedas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('conversor-de-moedas');
+    expect(app.title).toEqual('Conversor de Moedas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('conversor-de-moedas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Conversor de Moedas app is running!'
+    );
   });
 });
