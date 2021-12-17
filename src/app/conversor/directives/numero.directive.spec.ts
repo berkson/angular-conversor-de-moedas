d5ad15d45a5b152a605ed9df3f9a5a8a1748ea99
Berkson <berksonx@yahoo.com.br>
2021-12-17 00:12:53 -0300

    todos os testes estao ok

--- src/app/conversor/directives/numero.directive.spec.ts
+++ src/app/conversor/directives/numero.directive.spec.ts
@@ -1,8 +1,12 @@
+import createSpyObj = jasmine.createSpyObj;
+import { ElementRef } from '@angular/core';
 import { NumeroDirective } from './numero.directive';
+
+let element: ElementRef = createSpyObj('idNaicsRef', ['nativeElement']);
 
 describe('NumeroDirective', () => {
   it('should create an instance', () => {
-    const directive = new NumeroDirective();
+    const directive = new NumeroDirective(element);
     expect(directive).toBeTruthy();
   });
 });
